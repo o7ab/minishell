3922348468149a8a1995d0de1e8faba1dcbbca99
Omar Abusharib <oabushar@lab3r5s1.42abudhabi.ae>
2022-10-24 10:07:22 +0400

    Hello

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: oabushar <oabushar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 09:03:02 by oabushar          #+#    #+#             */
-/*   Updated: 2022/10/03 09:53:44 by oabushar         ###   ########.fr       */
+/*   Updated: 2022/10/08 20:45:52 by oabushar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,60 +18,120 @@ void	ft_exit()
 	exit(1);
 }
 
-// int	my_cmp(char *s1, char *s2, size_t n)
-// {
-// 	while (*s1 && *s2 && n > 0)
-// 	{
-// 		if (*s1 != *s2)
-// 		{
-// 			break ;
-// 		}
-// 		s1++;
-// 		s2++;
-// 		n--;
-// 	}
-// 	if (n <= 0)
-// 		return (0);
-// 	if ((unsigned char)*s1 - (unsigned char)*s2)
-// 	{
-// 		// printf("error %d, 1 is '%c', 2 is '%s'", (unsigned char)*s1 - (unsigned char)*s2, *s1, s2);
-// 		return(-420);
-// 	}
-// 	return ((unsigned char)*s1 - (unsigned char)*s2);
-// }
-
-int	check_valid(t_info *info)
+void	lst_add(t_node **list, int t, char *str)
 {
-	int		i;
-	char	*words;
+	t_node	*new;
+	t_node	*ptr;
 
-	i = 0;
-	words = "echo cd pwd unset exit export env";
-	if (!ft_strnstr(words, info->split[i], 34))
-		return (0);
-	if (!ft_strnstr(words, info->split[i], 34))
-		return (0);
-	if (!ft_strnstr(words, info->split[i], 34))
-		return (0);
-	if (!ft_strnstr(words, info->split[i], 34))
-		return (0);
-	if (!ft_strnstr(words, info->split[i], 34))
-		return (0);
-	if (!ft_strnstr(words, info->split[i], 34))
-		return (0);
-	if (!ft_strnstr(words, info->split[i], 34))
-		return (0);
-	return (1);
+	new = ((t_node *) malloc (sizeof(t_node)));
+	if (!new)
+		return ;
+	new->type = t;
+	new->next = NULL;
+	if (!(*list))
+		*list = new;
+	else
+	{
+		ptr = *list;
+		while (ptr->next)
+			ptr = ptr->next;
+		ptr->next = new;
+	}
 }
 
-int	parse_line(t_info *info)
+// int	check_valid(t_info *info)
+// {
+// 	int		i;
+// 	char	*words;
+
+// 	i = 0;
+// 	words = "echo cd pwd unset exit export env";
+// 	if (!ft_strnstr(words, info->split[i], 34))
+// 		return (0);
+// 	if (!ft_strnstr(words, info->split[i], 34))
+// 		return (0);
+// 	if (!ft_strnstr(words, info->split[i], 34))
+// 		return (0);
+// 	if (!ft_strnstr(words, info->split[i], 34))
+// 		return (0);
+// 	if (!ft_strnstr(words, info->split[i], 34))
+// 		return (0);
+// 	if (!ft_strnstr(words, info->split[i], 34))
+// 		return (0);
+// 	if (!ft_strnstr(words, info->split[i], 34))
+// 		return (0);
+// 	return (1);
+// }
+
+void	parse_space(t_node *list)
 {
-	t_node list;
-	
-	info->split = ft_split(info->line, ' ');
-	if (!check_valid(info))
+	lst_add(&list, SPACE, " ");
+}
+
+void parse_pipe(t_node *list)
+{
+	lst_add(&list, PIPE, "|");
+}
+
+char	*get_str(t_info *info, int index)
+{
+	char	*str;
+	int		i;
+	int		store;
+
+	i = 0;
+	store = index;
+	while (info->line[index] != 39 && info->line[index])
 	{
-		return (0);
+		i++;
+		index++;
+	}
+	str = (char *)malloc(sizeof(char) * i + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (info->line[store] != 39 && info->line[store])
+		str[i++] = info->line[store++];
+	
+}
+
+void	parse_quotes(t_node *list, t_info *info, int index)
+{
+	char *str;
+
+	if (info->line[index] == 39)
+	{
+		if (info->line[index] + 1 != 39)
+		{
+			str = get_str(info, index + 1);
+			lst_add(&list, QUOTES, info->line[index] + 1);
+		}
+			return;
+	}
+	else if (info->line[index] == '"')
+	{
+		if (info->line[index] + 1 != '"')
+		{
+			info->line[index];
+		}
+	}
+}
+int	parse_line(t_info *info, t_node *list)
+{
+	int		i;
+	
+	i = 0;
+	while (info->line[i])
+	{
+		if (info->line[i] == ' ')
+			parse_space(list);
+		else if (info->line[i] == '|')
+			parse_pipe(list);
+		else if (info->line[i] == 34 || info->line[i] == 39)
+			parse_quotes(list, info, i);
+		else if (ft_isprint(info->line[i]))
+			parse_word(info);
+		i++;
 	}
 	return (1);
 }
@@ -80,13 +140,11 @@ int main(void)
 {
 	// char buf[1024];
 	t_info info;
+	t_node list;
 	while (1)
 	{
 		printf("%s", PURPLE);
 		info.line = readline("minishell> \033[0m");
-		if (parse_line(&info))
-		{
-			
-		}
+		parse_line(&info, &list);
 	}
 }
