96bc3899490f1e6ef259b01ed452d333f1c06642
Omar Abusharib <oabushar@lab2r3s5.42abudhabi.ae>
2022-12-28 17:46:30 +0400

    calloced the structs and initiated

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: oabushar <oabushar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 09:03:02 by oabushar          #+#    #+#             */
-/*   Updated: 2022/12/28 16:35:14 by oabushar         ###   ########.fr       */
+/*   Updated: 2022/12/28 17:45:02 by oabushar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,20 @@ void	ft_exit()
 	exit(1);
 }
 
-void	parse_cmds(t_cmd *input, t_info *info)
+void	parse_cmds(t_cmd **input, t_info **info)
 {
 	int	i;
+	t_cmd *tmp;
 
 	i = 0;
-	while (i < info->n_cmd && info->split[i] && input)
+	tmp = *input;
+	while (i < (*info)->n_cmd && (*info)->split[i] && *input)
 	{
-		input->full_cmd = info->split[i++];
-		get_list(input, info);
-		input = input->next;
+		(*input)->full_cmd = (*info)->split[i++];
+		get_list(*input, *info);
+		*input = (*input)->next;
 	}
+	*input = tmp;
 }
 
 void	get_list(t_cmd *input, t_info *info)
@@ -53,39 +56,66 @@ void	get_list(t_cmd *input, t_info *info)
 	free_double(input->redir);
 }
 
-void	parse_line(t_info *info, t_cmd *input)
+void	parse_line(t_info **info, t_cmd **input)
 {
 	int i;
 
 	i = 0;
-	info->open_q = 0;
-	input = (NULL);
-	info->n_cmd = num_cmds(info);
-	info->split = ft_split_q(info->line, '|');
-	while (i < info->n_cmd)
+	(*info)->open_q = 0;
+	(*input) = (NULL);
+	(*info)->n_cmd = num_cmds(*info);
+	(*info)->split = ft_split_q((*info)->line, '|');
+	while (i < (*info)->n_cmd)
 	{
-		lst_add(&input);
+		lst_add(input);
 		i++;
 	}
 	parse_cmds(input, info);
-	i = 0;
+}
+
+void	test_func(t_cmd *input, t_info *info)
+{
+	int i = 0;
+	
 	while (input && input->s_cmd[i])
 	{
-		printf("the (%s)  \n", input->s_cmd[i++]);
+		printf("the      (%s)  \n", input->s_cmd[i++]);
+		printf("the     info  (%s)  \n", info->line);
 	}
+}
+
+void	init_struct(t_cmd *input, t_info *info)
+{
+	input->full_cmd = NULL;
+	input->full_op = NULL;
+	input->new_cmd = NULL;
+	input->s_cmd = NULL;
+	input->cmd = NULL;
+	input->files = NULL;
+	input->redir = NULL;
+	input->next = NULL;
+	info->line = NULL;
+	info->split = NULL;
+	info->n_cmd = 0;
+	info->open_q = 0;
 }
 
 int	main(void)
 {
 	// char buf[1024];
-	t_info	info;
-	t_cmd	input;
+	t_info	*info;
+	t_cmd	*input;
+
+	info = ft_calloc(1, sizeof(t_info));
+	input = ft_calloc(1, sizeof(t_cmd));
+	init_struct(input, info);
 	while (1)
 	{
 		printf("%s", PURPLE);
-		info.line = readline("minishell> \033[0m");
-		add_history(info.line);
+		info->line = readline("minishell> \033[0m");
+		add_history(info->line);
 		parse_line(&info, &input);
+		test_func(input, info);
 	}
 	return 0;
 }
